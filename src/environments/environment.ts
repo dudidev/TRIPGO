1f62cb8dcd45aaa2c0dd5b5d0f8708626a2504eb
Duvier David <duvierdiegodavid.0715@gmail.com>
2026-05-27 19:03:43 -0500

    fix(env): ajuste de nuevo URL para consumir backend rama develop, desplegado

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://api.tripgoapp.com'
-  //apiBaseUrl: 'https://api.staggin.tripgoapp.com' //pronto despliegue de pruebas
+  //apiBaseUrl: 'https://api.tripgoapp.com'
+  apiBaseUrl: 'https://tripgo-backend-staging.up.railway.app' //Link del backend develop desplegado para pruebas
   //apiBaseUrl: 'http://localhost:8080'
 };  
