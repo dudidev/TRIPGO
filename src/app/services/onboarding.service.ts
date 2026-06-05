51290b3a40e8bb51ea9d05b7fc76b89b3c592538
Duvansg06 <140823687+Duvansg06@users.noreply.github.com>
2026-06-04 19:07:36 -0500

    apto  para subir fotos

--- src/app/services/onboarding.service.ts
+++ src/app/services/onboarding.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 
 @Injectable({
@@ -7,22 +8,28 @@ import { environment } from '../../environments/environment';
 })
 export class OnboardingService {
 
-  constructor(
-    private http: HttpClient
-  ) {}
+  constructor(private http: HttpClient) {}
 
-  obtenerSolicitud(token: string) {
-
+  obtenerSolicitud(token: string): Observable<any> {
     return this.http.get(
       `${environment.apiBaseUrl}/onboarding/${token}`
     );
   }
 
-  completarSolicitud(
-    token: string,
-    payload: any
-  ) {
+  subirFotos(token: string, files: File[]): Observable<any> {
+    const formData = new FormData();
 
+    for (const file of files) {
+      formData.append('imagenes', file);
+    }
+
+    return this.http.post(
+      `${environment.apiBaseUrl}/onboarding/${token}/fotos`,
+      formData
+    );
+  }
+
+  completarSolicitud(token: string, payload: any): Observable<any> {
     return this.http.post(
       `${environment.apiBaseUrl}/onboarding/${token}/completar`,
       payload
